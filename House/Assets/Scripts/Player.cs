084dae462de128b05a937cc655db4088bb02b151
Nikandros <m.nik1997@hotmail.com>
2018-04-26 00:55:15 +0100

    Converted all the multivariables into arrays and player bodies are shown when game is started.

--- House/Assets/Scripts/Player.cs
+++ House/Assets/Scripts/Player.cs
@@ -4,11 +4,13 @@ public class Player
 {
 	private string name;
 	private int token;
+	private Boolean bot;
 
-	public Player (string name, int token)
+	public Player (string name, int token, Boolean bot)
 	{
 		this.name = name;
 		this.token = token;
+		this.bot = bot;
 	}
 
 	public int getToken() {
@@ -17,6 +19,10 @@ public class Player
 
 	public string getName() {
 		return name;
+	}
+
+	public Boolean isBot() {
+		return bot;
 	}
 }
 
