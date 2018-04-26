000636afa139bdb7e1013ac66a8e73fc3fbe023b
Nikandros <m.nik1997@hotmail.com>
2018-04-26 02:06:37 +0100

    deleted

--- House/Assets/Database/OnStart.sql
+++ House/Assets/Database/OnStart.sql
@@ -1,33 +1,33 @@
 INSERT INTO GameTable (
-	gameNo, time_, date_, trading)
-	VALUES (1, time('now'), date('now'), 1);
+	gameNo, time_, date_, trading) 
+	VALUES (1, time('now'), 1);
 
 INSERT INTO DevProperties (
-	tileNo, prop_name, group_, cost, undeveloped_rent, rent_1, rent_2, rent_3, rent_4, rent_5)
-	VALUES
-	(2,"Crapper Street","Brown",60,2,10,30,90,160,250),
-	(4,"Gangsters Paradise","Brown",60,4,20,60,180,320,450),
-	(7,"Weeping Angel","Blue",100,6,30,90,270,400,550),
-	(9,"Potts Avenue","Blue",100,6,30,90,270,400,550),
-	(10,"Nardole Drive","Blue",120,8,40,100,300,450,600),
-	(12,"Skywalker Drive","Purple",140,10,50,150,450,625,750),
-	(14,"Wookie Hole","Purple",140,10,50,150,450,625,750),
-	(15,"Rey Lane","Purple",160,12,60,180,500,700,900),
-	(17,"Cooper Drive","Orange",180,14,70,200,550,750,950),
-	(19,"Wolowitz Street","Orange",180,14,70,200,550,750,950),
-	(20,"Penny Lane","Orange",200,16,80,220,600,800,1000),
-	(22,"Yue Fei Square","Red",220,18,90,250,700,875,1050),
-	(24,"Mulan Rouge","Red",220,18,90,250,700,875,1050),
-	(25,"Han Xin Gardens","Red",240,20,100,300,750,925,1100),
-	(27,"Kirk Close","Yellow",260,22,110,330,800,975,1150),
-	(28,"Picard Avenue","Yellow",260,22,110,330,800,975,1150),
-	(30,"Crusher Creek","Yellow",280,22,120,360,850,1025,1200),
-	(32,"Sirat Mews","Green",300,26,130,390,900,1100,1275),
-	(33,"Ghengis Crescent","Green",300,26,130,390,900,1100,1275),
-	(35,"Ibis Close","Green",320,28,150,450,1000,1200,1400),
-	(38,"Hawking Way","Deep blue",350,35,175,500,1100,1300,1500),
-	(40,"Turing Heights","Deep blue",400,50,200,600,1400,1700,2000);
-
+	tileNo, prop_name, group_, cost, undeveloped_rent, undeveloped_rentAll, rent_1, rent_2, rent_3, rent_4, rent_5)
+	VALUES 
+	(2,"Crapper Street","Brown",60,2,4,10,30,90,160,250),
+	(4,"Gangsters Paradise","Brown",60,4,8,20,60,180,320,450),
+	(7,"Weeping Angel","Blue",100,6,12,30,90,270,400,550),
+	(9,"Potts Avenue","Blue",100,6,12,30,90,270,400,550),
+	(10,"Nardole Drive","Blue",120,8,16,40,100,300,450,600),
+	(12,"Skywalker Drive","Purple",140,10,20,50,150,450,625,750),
+	(14,"Wookie Hole","Purple",140,10,20,50,150,450,625,750),
+	(15,"Rey Lane","Purple",160,12,24,60,180,500,700,900),
+	(17,"Cooper Drive","Orange",180,14,28,70,200,550,750,950),
+	(19,"Wolowitz Street","Orange",180,14,28,70,200,550,750,950),
+	(20,"Penny Lane","Orange",200,16,32,80,220,600,800,1000),
+	(22,"Yue Fei Square","Red",220,18,36,90,250,700,875,1050),
+	(24,"Mulan Rouge","Red",220,18,36,90,250,700,875,1050),
+	(25,"Han Xin Gardens","Red",240,20,40,100,300,750,925,1100),
+	(27,"Kirk Close","Yellow",260,22,44,110,330,800,975,1150),
+	(28,"Picard Avenue","Yellow",260,22,44,110,330,800,975,1150),
+	(30,"Crusher Creek","Yellow",280,22,44,120,360,850,1025,1200),
+	(32,"Sirat Mews","Green",300,26,52,130,390,900,1100,1275),
+	(33,"Ghengis Crescent","Green",300,26,52,130,390,900,1100,1275),
+	(35,"Ibis Close","Green",320,28,56,150,450,1000,1200,1400),
+	(38,"Hawking Way","Deep blue",350,35,70,175,500,1100,1300,1500),
+	(40,"Turing Heights","Deep blue",400,50,100,200,600,1400,1700,2000);
+  
 INSERT INTO Stations (
 	tileNo, prop_name, group_, cost, rent1_St, rent2_St, rent3_St, rent4_St)
 	VALUES
@@ -39,7 +39,7 @@ INSERT INTO Stations (
 INSERT INTO Utilities (
 	tileNo, prop_name, group_, cost, rent1_Ut, rent2_Ut)
 	VALUES
-	(13, "Tesla Power Co", "Utilities", 150, 4, 10),
+	(13, "Tesla Power Co", "Utilities", 150, 4, 10), 
 	(29, "Edison Water", "Utilities", 150, 4, 10);
 
 INSERT INTO NonProperties (
@@ -58,8 +58,6 @@ INSERT INTO NonProperties (
 	(37,"Opportunity Knocks","OK Card",0),
 	(39,"Super Tax","Tax",100);
 
-
-
 INSERT INTO Players (
 	player_id, gameNo, token_loc, token_shape, wallet, get_out_of_jail_card, bot, player_name)
 	VALUES
@@ -72,6 +70,7 @@ INSERT INTO Account (
 
 INSERT INTO devAssets (
 	assetNo, developed, player_id, gameNo, mortgaged)
+
 	VALUES
 	(2,0,"BANK", 0),
 	(4,0,"BANK", 0),
@@ -110,48 +109,3 @@ INSERT INTO UtilitiesAssets (
 	(13,0,"BANK", 0),
 	(29,0,"BANK", 0);
 
-/*INSERT INTO Tiles (
-	tileNo, gameNo)
-	VALUES
-	(0, 1),
-	(1,	1),
-	(2,	1),
-	(3,	1),
-	(4,	1),
-	(5,	1),
-	(6,	1),
-	(7,	1),
-	(8,	1),
-	(9,	1),
-	(10,1),
-	(11,1),
-	(12,1),
-	(13,1),
-	(14,1),
-	(15,1),
-	(16,1),
-	(17,1),
-	(18,1),
-	(19,1),
-	(20,1),
-	(21,1),
-	(22,1),
-	(23,1),
-	(24,1),
-	(25,1),
-	(26,1),
-	(27,1),
-	(28,1),
-	(29,1),
-	(30,1),
-	(31,1),
-	(32,1),
-	(33,1),
-	(34,1),
-	(35,1),
-	(36,1),
-	(37,1),
-	(38,1),
-	(39,1),
-	(40,1)
-	*/
