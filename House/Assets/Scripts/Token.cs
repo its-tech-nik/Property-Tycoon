3768a774fd55fbfbbb5b550b91e5e51de48cec54
Nikandros <m.nik1997@hotmail.com>
2018-04-27 05:45:02 +0100

    Token class created.

---
+++ House/Assets/Scripts/Token.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Token {
+
+	private int position;
+	private bool passedGo;
+
+	public Token () {
+		position = 0;
+		passedGo = false;
+	}
+
+	/**
+     * Moves the token on the board.
+     * 
+     * @param position The amount of positions a token needs to be moved.
+     * @return true if it has passed go, false otherwise.
+     */
+	public bool movePiece(int positions) {
+		position += positions;
+
+		if(position >= 40) {
+			position -= 40;
+			return passedGo = true;
+		}
+		return false;
+	}
+
+
+}
+
